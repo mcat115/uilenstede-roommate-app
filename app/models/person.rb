d6606ab8c9cde85373020796bed186cc6f561452
Michael Catanese <michaeljcatanese@gmail.com>
2022-09-13 21:54:28 +0200

    fixed typo

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ActiveRecord::Base
-  validates :name, presence: true, length: {minimum: 1}
-  validates :trash_score, presence: true
-  validates :clean_score, presence: true
-  validates :country_team
+  validate :name, presence: true, length: {minimum: 1}
+  validate :trash_score, presence: true
+  validate :clean_score, presence: true
+  validate :country_team
 end
